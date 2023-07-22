2e59ef4beed5ac68f07e9c2b275c38c08f2a2857
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-22 09:28:54 -0300

    modifie contract by my study

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-contract MeuContrato {
-    // Uma variável de estado que armazena um número
-    uint public meuNumero;
+contract BasicContract {
+    uint private number;
 
-    // Função que altera o valor da variável de estado
-    function setMeuNumero(uint _meuNumero) public {
-        meuNumero = _meuNumero;
+    event AlterNumber(uint novoNumero);
+
+    function defineNumber(uint _numero) public {
+        number = _numero;
+        emit AlterNumber(_numero);
+    }
+
+    function getNumber () public view returns(uint) {
+        return number;
     }
 }
