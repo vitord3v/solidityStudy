8ca1e2e647d3e176b11317a076a2efba2f032c55
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-22 09:27:54 -0300

    [fix] localhost port

--- truffle-config.js
+++ truffle-config.js
@@ -2,14 +2,14 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",     // Endereço do Ganache
-      port: 7545,            // Porta RPC do Ganache
+      port: 8545,            // Porta RPC do Ganache
       network_id: "*",       // Corresponde a qualquer id de rede
     },
   },
   // Configuração do compilador Solidity
   compilers: {
     solc: {
-      version: "0.8.4",      // Versão do compilador Solidity a ser usada
+      version: "0.8.6",      // Versão do compilador Solidity a ser usada
     }
   }
 };
